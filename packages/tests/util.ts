74cddd5004c6db0baca270423fb64fdb7e504c31
crustydev <ademola1145@gmail.com>
2023-03-01 23:59:30 +0100

    Update content urls

--- packages/tests/util.ts
+++ packages/tests/util.ts
@@ -1,7 +1,7 @@
 export const getTestMetadata = (): Array<string> => {
   return [
-    "https://raw.githubusercontent.com/sunrise-stake/mint-program/ademola/packages/tests/fixtures/metadata/meta1.json",
-    "https://raw.githubusercontent.com/sunrise-stake/mint-program/ademola/packages/tests/fixtures/metadata/meta2.json",
-    "https://raw.githubusercontent.com/sunrise-stake/mint-program/ademola/packages/tests/fixtures/metadata/meta3.json",
+    "https://raw.githubusercontent.com/sunrise-stake/mint-program/active/packages/tests/fixtures/metadata/meta1.json",
+    "https://raw.githubusercontent.com/sunrise-stake/mint-program/active/packages/tests/fixtures/metadata/meta2.json",
+    "https://raw.githubusercontent.com/sunrise-stake/mint-program/active/packages/tests/fixtures/metadata/meta3.json",
   ];
 }
