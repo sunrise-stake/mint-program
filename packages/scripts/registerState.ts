81bda7df9004018a311aad37737826c06c9eab24
dankelleher <dankelleher@yahoo.com>
2023-03-03 12:50:15 +0100

    Restructured client in order to be publishable and added a script to register state

---
+++ packages/scripts/registerState.ts
@@ -0,0 +1,10 @@
+import {ImpactNftClient} from "../client/src";
+
+// USAGE
+// ANCHOR_WALLET=~/.config/solana/id.json ANCHOR_PROVIDER_URL=https://api.devnet.solana.com yarn ts-node packages/scripts/registerState.ts
+
+const levels = 10;
+(async () => {
+    const client = await ImpactNftClient.register(levels);
+    console.log(client.stateAddress);
+})();
