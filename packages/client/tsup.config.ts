8859591e5700c608b1351aa8284e3058f1dd16f3
dankelleher <dankelleher@yahoo.com>
2023-03-12 07:29:58 +0100

    Switch to tsup for the build

--- packages/client/tsup.config.ts
+++ packages/client/tsup.config.ts
@@ -1,17 +1,14 @@
 import type { Options } from 'tsup';
 
-const env = process.env.NODE_ENV;
-
 export const tsup: Options = {
     splitting: true,
     clean: true, // clean up the dist folder
     dts: true, // generate dts files
     format: ['cjs', 'esm'], // generate cjs and esm files
-    minify: env === 'production',
-    bundle: env === 'production',
+    minify: true,
+    bundle: false,
     skipNodeModulesBundle: true,
-    entryPoints: ['src/index.ts'],
-    watch: env === 'development',
+    watch: false,
     target: 'es2020',
     outDir: 'dist',
     entry: ['src/**/*.ts'], //include all files under src
